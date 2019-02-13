563868b8c5505d472a7fa896c51655e397a6ecff
王平安 <pingan8787@qq.com>
2019-02-13 21:57:02 +0800

    2019.02.13

--- 0-demo/3-get_all_file.js
+++ 0-demo/3-get_all_file.js
@@ -8,22 +8,26 @@
  * 同事两行泪。
  */
 
-/**
+ /**
  * 用途：
  * 检索项目中包含指定标签或类名的文件，并返回文件路径列表。
  * 检索方式：查找指定的标签或者类名两种方式。
  */
 
-var fs = require('fs');
+var fs   = require('fs');
 var path = require('path');
 
 var filterFile = ['.html'];  // 需要检索的文件类型
+var filterDir  = ['lib'];    // 需要排除的文件夹
 var labelArray = [''];       // 需要检索的标签数组 （暂不支持）
-var classArray = ['leo_class','leo_name'];  // 需要检索的类名数组
-var filePath = path.join(__dirname, '..', 'develop');
-var allFileNumber = 0;       // 总文件数
-var resultArray = [];        // 最终结果
+var classArray = [           // 需要检索的类名数组 （如果是标签的话 可以加个<符号表示标签开始）
+	'leo_class','leo_name','leo_height'
+];  
+var filePath       = path.join(__dirname, '..', 'develop');        // 需要检索的文件夹路径
+var allFileNumber  = 0;      // 总文件数
+var resultArray    = [];     // 最终结果
 var resultAlassify = {};     // 最终结果分类
+var saveFileName   = 'search_current_file_' + Date.now() + '.txt'; // 保存文件名
 
 
 /**
@@ -41,7 +45,7 @@ var getCurrenAllFile = function (paths){
 			var c_path = path.join(paths, item);
 			var stat = fs.lstatSync(c_path);
 			if(stat && stat.isDirectory()){
-				getCurrenAllFile(c_path);
+				if(filterDir.indexOf(item) < 0) getCurrenAllFile(c_path); // 排除不需要检索的文件夹
 			}else{
 				if(filterFile.indexOf(path.extname(item)) >= 0 ){
 					getCurrentFile(c_path, item);
@@ -87,20 +91,44 @@ var searchCurrentFile = function(data, paths){
 	}
 };
 
+/**
+ * 对结果进行去重
+ * @param {*} arr  结果的数据
+ */
+var unique = function (arr){
+    var res=[];
+    for(var i=0,len=arr.length;i<len;i++){
+        var obj = arr[i];
+        for(var j=0,jlen = res.length;j<jlen;j++){
+            if(res[j]===obj) break;            
+        }
+        if(jlen===j)res.push(obj);
+    }
+    return res;
+}
 
+/**
+ * 初始化项目
+ */
 var init = function (){
 	getCurrenAllFile(filePath);
 	setTimeout(function(){
-		// 最后输出
-		console.log('-------检索结果-------')
-		console.log('共检索到：'+resultArray.length+'个结果！');
-		console.log(resultArray);
-		console.log('---------------------')
+		var text = '';
+		resultArray = unique(resultArray);// 对结果进行去重
+		text = '-----------------所有检索结果（已去重）（共' + resultArray.length+ '个）----------------------\r\n' 
+		     + resultArray.toString().replace(/,/g,'\r\n') + '\r\n'
+		     + '------------------------------------------------\r\n';
 		for(var k in classArray){
-			console.log('-------' + classArray[k] + '检索结果（共' + resultAlassify[classArray[k]].length+ '个）-------')
-			console.log(resultAlassify[classArray[k]]);
-			console.log('---------------------')
+			text = text 
+			     + '-------关键词【' + classArray[k] + '】检索结果（共' + resultAlassify[classArray[k]].length+ '个）-------\r\n'
+				 + resultAlassify[classArray[k]].toString().replace(/,/g,'\r\n') + '\r\n'
+				 + '------------------------------------------------\r\n';
 		}
+		console.log(text)
+		fs.writeFile(saveFileName , text, 'utf8' , function(err){
+			if(err) console.log(err);
+			console.log('检索结果文件保存成功！');
+		})
 	}, 1000);
 };
 
